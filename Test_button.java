5d148e751fd0ebec5941069abb571ca9ab170e48
AnselotSteven <102813908+AnselotSteven@users.noreply.github.com>
2022-05-31 10:10:01 +0200

    pour les boutons

---
+++ Test_button.java
@@ -0,0 +1,29 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Test_button extends Application{
+
+	@Override
+	public void start(Stage stage) throws Exception {
+	
+		Group group = new Group();
+		Scene scene = new Scene(group, 200,150);
+		Button button = new Button();
+
+		
+		group.getChildren().add(button);
+		stage.setScene(scene);;
+		stage.show();
+	}
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+	}
+}
